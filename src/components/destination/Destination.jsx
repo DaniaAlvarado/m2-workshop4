c65256ba6c4b5b36d77ed9f6ae7220d3547395a3
DaniaAlvarado <dania26alvarado@gmail.com>
2022-10-24 18:52:46 -0500

    destination desing

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Background from '../../assets/destination/background-destination-desktop.jpg';
+import { getDestinations } from "../../services/getApi";
+import DesLink from "../destinationLink/DesLink";
 import Header from "../header/Header";
-import './Destination.scss'
+import './Destination.scss';
+import moon from '../../assets/destination/image-moon.webp';
 
 const Destination = () => {
+
+    const [destinationInfo, setDestinationInfo] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() =>{
+        getDestInfo ();
+    }, []);
+
+    const getDestInfo =  async () =>{
+        const [destination] =  await Promise.all([getDestinations()])
+        setDestinationInfo(destination)
+        setLoading(false)
+    }
+    
     return (
         <section className="destination">
             <div className="destination__background">
                 <img src={Background} alt="Destination background" />
             </div>
             <Header />
+            <article className="destination__main">
+                <aside className="destination__main__left">
+                    <span>PICK YOUR DESTINATION</span>
+                    <img src={moon} alt="destination" />
+                </aside>
+                <aside className="destination__main__right">
+                    <DesLink />
+                    <span className="title1">
+                        MOON
+                    {/* {
+                        !loading ? destinationInfo.map((element, index) => (
+                            <div key={index}>{element.name}</div>
+                        )) : (<div>Cragando</div>)
+                    } */}
+                    </span>
+                    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium in ratione, laboriosam provident eum ea vitae repellendus. Ratione error nulla nesciunt neque excepturi, rerum facere, minima, odio sit sint sunt.</p>
+                    <nav>
+                        <div>
+                            <span className="title">AVG. DISTANCE</span>
+                            <span></span>
+                        </div>
+                        <div>
+                            <span className="title">EST. TRAVEL TIME</span>
+                            <span></span>
+                        </div>
+                    </nav>
+                </aside>
+            </article>
         </section>
     )
 }
--- src/components/destinationLink/DesLink.jsx
+++ src/components/destinationLink/DesLink.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DesLink = () => {
-    return(
-        <div>
-            links destination
-        </div>
+    return (
+            <ul className="destination__main__menu">
+                <li className="destination__main__menu__item">
+                    <Link to="/"
+                        style={{ textDecoration: 'none' }}>
+                        <span>MOON</span>
+                    </Link>
+                </li>
+                <li className="destination__menu__item">
+                    <Link to="/"
+                        style={{ textDecoration: 'none' }}>
+                        <span>MARS</span>
+                    </Link>
+                </li>
+                <li className="destination__menu__item">
+                    <Link to="/"
+                        style={{ textDecoration: 'none' }}>
+                        <span>EUROPA</span>
+                    </Link>
+                </li>
+                <li className="destination__menu__item">
+                    <Link to="/"
+                        style={{ textDecoration: 'none' }}>
+                        <span>TITAN</span>
+                    </Link>
+                </li>
+            </ul>
     )
 }
 
