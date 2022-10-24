cf082e86547c5cf58497676906ade47819b2c2b5
davidsilva131 <davidsilvac131@gmail.com>
2022-10-24 09:51:18 -0500

    Adding router file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './components/home/Home';
+import { Router } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Home />
-
+  <Router />
 );
 
---
+++ src/routes/Router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../components/home/Home";
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
